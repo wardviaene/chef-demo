8ac7b723507ad08bc7d33cc0740012175775f2e3
Edward Viaene <ward.viaene@gmail.com>
2015-11-09 15:52:54 +0100

    docker machine

---
+++ misc/docker-machine.rb
@@ -0,0 +1,18 @@
+require 'chef/provisioning/docker_driver'
+
+machine_image 'webserver' do
+  recipe 'my_cookbook'
+
+  machine_options :docker_options => {
+      :base_image => {
+          :name => 'ubuntu',
+          :repository => 'ubuntu',
+          :tag => '14.04'
+      }
+       :ports => "8080:80,
+  }
+end
+
+machine 'webserver1' do
+  from_image 'webserver'
+end
